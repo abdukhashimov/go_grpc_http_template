67858179d1aba440910e4db5a9969d0947ca5335
Madiyor <abdukhashimov@yandex.ru>
2022-09-26 08:59:54 +0500

    SA-11 Add custom logger

--- pkg/logger/config.go
+++ pkg/logger/config.go
@@ -1,7 +1,9 @@
 package logger
 
 type Options struct {
-	ProjectName string `env:"PROJECT_NAME,default=golang_project"`
-	DevMode     string `env:"DEV_MODE,default=false"`
-	LogLevel    string `env:"LOG_LEVEL,default=info"`
+	ProjectName    string `env:"PROJECT_NAME,default=golang_project"`
+	DevMode        bool   `env:"DEV_MODE,default=false"`
+	LogLevel       string `env:"LOG_LEVEL,default=info"`
+	DateTimeFormat string `env:"DATE_TIME_FORMAT,default:2006-01-02 15:04:05"`
+	DateFormat     string `env:"DATE_FORMAT,default:2006-01-02"`
 }
