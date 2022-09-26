62776e7e590ddbc63d924729edeb4bcb64d44491
Madiyor <abdukhashimov@yandex.ru>
2022-09-26 08:40:15 +0500

    SA-11 Add log config

--- pkg/logger/config.go
+++ pkg/logger/config.go
@@ -2,5 +2,6 @@ package logger
 
 type Options struct {
 	ProjectName string `env:"PROJECT_NAME,default=golang_project"`
-	Environment string `env:"ENVIRONMENT"`
+	DevMode     string `env:"DEV_MODE,default=false"`
+	LogLevel    string `env:"LOG_LEVEL,default=info"`
 }
