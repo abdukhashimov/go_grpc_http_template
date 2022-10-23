b812d15ecf44be8633b061a13da86c68a4e357b5
Madiyor <abdukhashimov@yandex.ru>
2022-10-23 22:34:18 +0500

    Add ports example

---
+++ internal/core/ports/messages.go
@@ -0,0 +1,15 @@
+package ports
+
+import (
+	"context"
+
+	v1 "github.com/abdukhashimov/go_api/internal/common/gen/go/user/service/v1"
+)
+
+type MessagesService interface {
+	Create(ctx context.Context, input v1.CreateMessageRequest) (*v1.Message, error)
+}
+
+type MessagesStore interface {
+	Create(ctx context.Context, input v1.CreateMessageRequest) (string, error)
+}
